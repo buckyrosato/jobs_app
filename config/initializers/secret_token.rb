7d68d11c75c617c1a5ef8cce95aebc477266274b
Bucky Rosato <b.t.rosato@gmail.com>
2011-09-08 19:15:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobsApp::Application.config.secret_token = 'd5982ef38d7bcac9f78d496f48fb4f49d69ab66135b368e1c19c09ac75941c8a5dc689184f60edfce0b8732ffb55ecd35d85c2b569c51afaaf0dd846b8beb27f'
